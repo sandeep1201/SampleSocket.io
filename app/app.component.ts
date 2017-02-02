0c6ca87a9a94e95bd95ad334c8f54880aba748b3
Alalla, SandeepReddy <SALALLA@amfam.com>
2017-02-02 17:34:10 -0600

    Second Commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service'
+@Component({
+    moduleId:module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    providers:[ChatService]
+})
+
+export class AppComponent { }
